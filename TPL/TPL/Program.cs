8ebf189f951f048ce2ad023042e9b188d1e1b366
wayne <profbat018@gmail.com>
2025-04-01 17:12:05 +0400

    SQL lesson 4-6

---
+++ TPL/TPL/Program.cs
@@ -0,0 +1,43 @@
+﻿#region Part1
+
+// Console.WriteLine("Start of Main Thread");
+//
+// Task.Run(() =>
+// {
+//     Console.WriteLine("Start of Task 1");
+//     Thread.Sleep(2000);
+//     Console.WriteLine("End of Task 1");
+// });
+//
+//
+// Console.WriteLine("End of Main Thread");
+
+#endregion
+
+
+#region Part2
+
+Console.WriteLine("Start of Main Thread");
+
+Task foo()
+{
+    Console.WriteLine("Start of Task 1");
+    
+    // Task.Delay(2000).Wait();
+    
+    Console.WriteLine("End of Task 1");
+    
+    return Task.CompletedTask;
+}
+
+foo();
+
+Console.WriteLine("End of Main Thread");
+
+
+
+
+
+
+
+#endregion
---
+++ ThreadPoolExample/ThreadPoolExample/Program.cs
@@ -0,0 +1,38 @@
+﻿#region Part1
+
+var counter = 0;
+object lockObject = new();
+
+
+ThreadPool.QueueUserWorkItem((state) =>
+{
+    lock (lockObject)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            counter++;
+            Console.WriteLine(
+                $"Thread: {Thread.CurrentThread}, IsThreadPool: {Thread.CurrentThread.IsThreadPoolThread}, Counter = {counter}");
+        }
+    }
+});
+
+
+Thread th1 = new(() =>
+{
+    lock (lockObject)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            counter++;
+            Console.WriteLine(
+                $"Thread: {Thread.CurrentThread}, IsThreadPool: {Thread.CurrentThread.IsThreadPoolThread}, Counter = {counter}");
+        }
+    }
+});
+
+th1.Start();
+
+#endregion
+
+// Console.WriteLine(ThreadPool.ThreadCount);
