4fff05bb32ef3fc7ec4908c0e2065fd1034d257b
wayne <profbat018@gmail.com>
2025-04-22 16:12:36 +0400

    updated

--- CatingTypes/MultiClient/Program.cs
+++ CatingTypes/MultiClient/Program.cs
@@ -6,21 +6,37 @@ using System.Text;
 int MulticastPort = 5000;
 string MulticastGroupAddress = "239.0.0.222";
 
+var localAddress = GetLocalIPAddress();
 
 using var udpClient = new UdpClient();
 
 udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
 udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, MulticastPort));
 
-udpClient.JoinMulticastGroup(IPAddress.Parse(MulticastGroupAddress));
+udpClient.JoinMulticastGroup(IPAddress.Parse(MulticastGroupAddress), localAddress);
 
 Console.WriteLine("Multicast Client запущен. Ожидание сообщений...");
 
 while (true)
 {
+    byte[] buffer = new byte[1024];
     IPEndPoint remoteEndPoint = new(IPAddress.Any, 0);
-    byte[] buffer = udpClient.Receive(ref remoteEndPoint);
+    buffer = udpClient.Receive(ref remoteEndPoint);
     string message = Encoding.UTF8.GetString(buffer);
 
     Console.WriteLine($"[Получено от {remoteEndPoint}]: {message}");
+}
+
+
+static IPAddress GetLocalIPAddress()
+{
+    foreach (var netInterface in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+    {
+        if (netInterface.AddressFamily == AddressFamily.InterNetwork)
+        {
+            return netInterface;
+        }
+    }
+
+    throw new Exception("Local IPv4 address not found!");
 }
